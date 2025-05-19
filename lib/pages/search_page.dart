708c75645d0b8e1b728d11b644b7c1f7569fc788
kurkobein <felipe.sanchez4@cloud.uautonoma.cl>
2025-05-19 15:46:26 -0400

    ahora no aparce el usuario actual y se puede colocar una imagen por default

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -11,6 +12,7 @@ class _BuscadorUsuariosState extends State<BuscadorUsuarios> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(title: Text('Buscar Usuarios')),
       body: Column(
@@ -38,15 +40,20 @@ class _BuscadorUsuariosState extends State<BuscadorUsuarios> {
 
                 if (!snapshot.hasData || snapshot.data!.docs.isEmpty)
                   return Center(child: Text('No se encontraron usuarios.'));
-
+                final String uidActual = FirebaseAuth.instance.currentUser!.uid;
                 final usuarios = snapshot.data!.docs;
+                final usuariosFiltrados = usuarios.where((u) => u.id != uidActual).toList();
+
                 return ListView.builder(
-                  itemCount: usuarios.length,
+                  itemCount: usuariosFiltrados.length,
                   itemBuilder: (context, index) {
-                    final usuario = usuarios[index];
+                    final usuario = usuariosFiltrados[index];
                     return ListTile(
                       leading: CircleAvatar(
-                        backgroundImage: NetworkImage(usuario['fotoPerfilUrl'] ?? ''),
+                        backgroundImage: NetworkImage(
+                          usuario['fotoPerfilUrl'] ??
+                          'https://qehohrpghlqjkatoyqux.supabase.co/storage/v1/object/public/imagenes/fotos_defecto/default.png',
+                        ),
                       ),
                       title: Text(usuario['usuario'] ?? 'Sin usuario'),
                       subtitle: Text(usuario['nombre'] ?? ''),
@@ -58,7 +65,6 @@ class _BuscadorUsuariosState extends State<BuscadorUsuarios> {
                         );
                       },
                     );
-
                   },
                 );
               },
